d1986ebf290f81a145377c5918660d315fe785ae
Kyostenas <kyostenas@gmail.com>
2025-07-22 16:51:21 -0600

    chore: prueba

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { APP_BASE_HREF, CommonModule, JsonPipe } from '@angular/common';
+import { Component, LOCALE_ID, signal } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true,
+  providers: [
+      // Configuraciones de idioma.
+      { provide: APP_BASE_HREF, useValue: '/' },
+      { provide: LOCALE_ID, useValue: 'es-MX' },
+  ],
 })
 export class App {
   protected readonly title = signal('carrduci-sys-online');
+  
+  prueba = environment.__;
 }
